c573946a8cff39b64d2da26fb70a522113c73453
gonzam88 <gonzamoiguer@gmail.com>
2021-04-15 21:46:22 -0300

    Terminado (sin estilo) programacion de capsula La seigneurie. Bug que si volves a comenzar no estan reseteados los valores

---
+++ inc/connect-lines.js
@@ -0,0 +1,136 @@
+// by gonzalo moiguer
+// gonzamoiguer.com
+var ConnectLines = {
+    mousePos: {x: 0, y: 0},
+    currKey : "",
+    isDrawingLine : false,
+    drawingFrom : null,
+    myData : {},
+    requestId: null,
+
+
+    CheckAllGuessed: function(){
+        let entries = Object.entries(this.myData)
+        let sum = 0
+        for(let i = 0; i < entries.length; i++){
+            sum += entries[i][1].guessed
+        }
+        if(sum == entries.length){
+            console.log("Juego ganado!")
+            this.Stop();
+        }
+    },
+
+    GetStyleData: function(x1, y1, x2, y2) {
+        var a = x1 - x2,
+            b = y1 - y2,
+            c = Math.sqrt(a * a + b * b);
+        var sx = (x1 + x2) / 2,
+            sy = (y1 + y2) / 2;
+        var x = sx - c / 2,
+            y = sy;
+        var alpha = Math.PI - Math.atan2(-b, a);
+        return {x:x, y:y, length:c, angle:alpha}
+    },
+
+    DragStarted: function(key, element){
+        if(this.myData[key].guessed){ return }
+        this.myData[key].line.classList.remove("lineHide")
+        this.drawingFrom = element
+        this.currKey = key
+        this.isDrawingLine = true
+    },
+
+    Draw: function(){
+        var _this = this;
+        if(_this.isDrawingLine){
+            let fromRect = _this.drawingFrom.getBoundingClientRect()
+            let data = _this.GetStyleData(fromRect.x+fromRect.width-5, fromRect.y+(fromRect.width/2), _this.mousePos.x, _this.mousePos.y)
+
+            var styles = 'width: ' + data.length + 'px; '
+                       + 'height: 0px; '
+                       + '-moz-transform: rotate(' + data.angle + 'rad); '
+                       + '-webkit-transform: rotate(' + data.angle + 'rad); '
+                       + '-o-transform: rotate(' + data.angle + 'rad); '
+                       + '-ms-transform: rotate(' + data.angle + 'rad); '
+                       + 'top: ' + data.y + 'px; '
+                       + 'left: ' + data.x + 'px; ';
+            _this.myData[_this.currKey].line.setAttribute('style', styles);
+        }
+
+        _this.requestId = requestAnimationFrame(function(){_this.Draw()})
+    },
+
+    // ConnectLines.Init(".from > div")
+    Init:function(fromsQuery){
+        var _this = this;
+        this.isDrawingLine = false
+        this.myData = {}
+        this.requestId = null
+        // updated mouse position
+        document.onmousemove = (event) => {
+            var eventDoc, doc, body;
+            event = event || window.event; // IE-ism
+            // If pageX/Y aren't available and clientX/Y are,
+            // calculate pageX/Y - logic taken from jQuery.
+            // (This is to support old IE)
+            if (event.pageX == null && event.clientX != null) {
+                eventDoc = (event.target && event.target.ownerDocument) || document;
+                doc = eventDoc.documentElement;
+                body = eventDoc.body;
+
+                event.pageX = event.clientX +
+                  (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+                  (doc && doc.clientLeft || body && body.clientLeft || 0);
+                event.pageY = event.clientY +
+                  (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+                  (doc && doc.clientTop  || body && body.clientTop  || 0 );
+            }
+            this.mousePos={x: event.pageX, y: event.pageY}
+        }
+        // create base data
+        froms = document.querySelectorAll(fromsQuery)
+        for(let i=0; i<froms.length; i++){
+            let myKey = froms[i].dataset.key
+            let to = document.querySelector(`.to > div[data-key=${myKey}]`)
+
+            let myLine = document.createElement("div");
+            myLine.setAttribute('class', "arrow");
+            myLine.classList.add("lineHide")
+            document.body.appendChild(myLine)
+
+            froms[i].addEventListener("mousedown", function(e){
+                _this.DragStarted(myKey, froms[i])
+            })
+            this.myData[myKey] = {from: froms[i], to: to, line:myLine, guessed: false}
+        }
+        // Mouseup eventlistener
+        document.addEventListener("mouseup", function(e){
+            if(_this.isDrawingLine){
+                if(!e.target.classList.contains("boxto")){
+                    _this.myData[_this.currKey].line.classList.add("lineHide")
+                }else{
+                    _this.myData[_this.currKey].line.classList.remove("lineHide")
+                    let droppedKey = e.target.dataset.key;
+                    if(_this.currKey == droppedKey){
+                        // console.log("Correct")
+                        _this.myData[_this.currKey].line.classList.add("correct")
+                        _this.myData[_this.currKey].from.classList.add("correct")
+                        _this.myData[_this.currKey].to.classList.add("correct")
+                        _this.myData[_this.currKey].guessed = true
+                        _this.CheckAllGuessed()
+                    }
+                }
+                _this.isDrawingLine = false
+            }
+        })
+        this.Draw()
+    },
+
+    Stop: function(){
+        window.cancelAnimationFrame(this.requestId);
+        this.requestId = undefined;
+        document.onmousemove = null
+
+    }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -6,78 +6,201 @@ document.addEventListener("DOMContentLoaded", () => {
     app = new Vue({
         el: '#app',
         data: {
-            capsule:"menu",
-            game:0,
-            colors:{
-                seigneurie:"#A5EA1B",
-                voyageurs:"#F8375B",
-                moulins:"#F2B120",
-                moderne:"#A3C4E7",
+            capsule: "menu",
+            game: 0,
+            colors: {
+                seigneurie: "#A5EA1B",
+                voyageurs: "#F8375B",
+                moulins: "#F2B120",
+                moderne: "#A3C4E7",
             },
-            games:{
-                menu:[
-                    {title: "Jeux interactifs"}
-                ],
-                seigneurie:[{
-                    // Game #1 LES OBJETS DU QUOTIDIEN
+            message:{
+                show: false,
+                title: "",
+
+            },
+            games: {
+                menu: [{
+                    title: "Jeux interactifs"
+                }],
+                seigneurie: [{
+                    // seigneurie Game #1 LES OBJETS DU QUOTIDIEN
                     title: "JEU #1 – LES OBJETS DU QUOTIDIEN: NOUVELLE-FRANCE",
-                    objects:[
-                        {name: "auto", correct:false, guessed:false},
-                        {name: "rabot", correct:true, guessed:false},
-                        {name: "walkman", correct:false, guessed:false},
-                        {name: "ecrire", correct:false, guessed:false},
-                        {name: "raquette", correct:true, guessed:false},
-                        {name: "ampoule", correct:false, guessed:false},
-                        {name: "chapelet", correct:true, guessed:false},
-                        {name: "faux", correct:true, guessed:false},
-                        {name: "moulin", correct:true, guessed:false},
-                        {name: "telephone", correct:false, guessed:false}
+                    objects: [
+                        {
+                            name: "auto",
+                            correct: false,
+                            guessed: false
+                        },
+                        {
+                            name: "rabot",
+                            correct: true,
+                            guessed: false
+                        },
+                        {
+                            name: "walkman",
+                            correct: false,
+                            guessed: false
+                        },
+                        {
+                            name: "ecrire",
+                            correct: false,
+                            guessed: false
+                        },
+                        {
+                            name: "raquette",
+                            correct: true,
+                            guessed: false
+                        },
+                        {
+                            name: "ampoule",
+                            correct: false,
+                            guessed: false
+                        },
+                        {
+                            name: "chapelet",
+                            correct: true,
+                            guessed: false
+                        },
+                        {
+                            name: "faux",
+                            correct: true,
+                            guessed: false
+                        },
+                        {
+                            name: "moulin",
+                            correct: true,
+                            guessed: false
+                        },
+                        {
+                            name: "telephone",
+                            correct: false,
+                            guessed: false
+                        }
                     ]
-                },{
-                    // game #2
-                },{
-                    // game #3
+                }, {
+                    // seigneurie Game #2
+                    title: "QUIZ HISTORIQUE",
+                    winMessage: "Ganasteee el segundo juego",
+                    question : 0,
+                    objects:[
+                        {q: "En moyenne, combien d’enfants retrouve-t-on par famille?", img: "a.jpg"},
+                        {q: "Au temps du régime seigneurial, on organise souvent des charivaris. Pourquoi? ", img: "a.jpg"},
+                        {q: "Les premières seigneuries font face à un cours d’eau. Celle de Terrebonne longe la rivière des Mille-Îles, autrefois nommée", img: "a.jpg"},
+                        {q: "Les censitaires peuvent-ils voter?", img: "a.jpg"},
+                        {q: "Selon l’état et L’Église en Nouvelle-France, un bon habitant est un ______________ pratiquant et obéissant, attaché à sa langue et qui cultive sa terre en travaillant dur.", img: "a.jpg"},
+                        {q: "Quand le censitaire fait moudre son blé au moulin, il doit donner une partie au seigneur. Combien de sac de blé doit-il donner au seigneur?", img: "a.jpg"},
+                    ]
+                }, {
+                    // seigneurie Game #3
+                    title: "SEIGNEUR OU CENSITAIRE?",
+                    winMessage: this.seigneurieJeu3Win,
+                    score: 0,
+                    question : 0,
+                    objects:[
+                        {q: "Il a toujours le premier banc à l’église. ", r: "seigneur"},
+                        {q: "Il peut donner sa terre à ses enfants ou la vendre. ", r: "censitaire"},
+                        {q: "Il peut avoir son propre four. ", r: "censitaire" },
+                        {q: "Il doit construire un moulin à farine. ", r: "seigneur"},
+                        {q: "Il peut chasser et pêcher pour nourrir sa famille.", r:"censitaire" },
+                        {q: "Il doit jurer fidélité au roi.", r: "seigneur"}
+                    ]
                 }],
 
             }
         },
 
         computed: {
-            GetTitle:function(){
-                if(this.capsule == "menu"){
+            GetTitle: function() {
+                if (this.capsule == "menu") {
                     return this.games.menu[0].title
-                }else{
-                    return this.games[this.capsule][this.game-1].title
+                } else {
+                    return this.games[this.capsule][this.game - 1].title
                 }
+            },
+            seigneurieJeu3Win: function(){
+                return `Your score is ${this.games.seigneurie[2].score}/${this.games.seigneurie[2].objects.length}`
             }
+
+
+
         },
 
         watch: {},
 
 
         methods: {
-            CheckElementIsCorrect(el,ev){
-                // console.log(ev.currentTarget)
-                let myObjects = this.games[this.capsule][this.game-1].objects
+            CheckElementIsCorrect(el, ev) {
+                let myObjects = this.games[this.capsule][this.game - 1].objects
                 let obj = myObjects.find(x => x.name == el)
 
-                if(obj.correct){
-                    console.log("correct :)")
+                let dom = ev.currentTarget
+                if (obj.correct) {
+                    // console.log("correct :)")
                     SoundCorrect()
                     obj.guessed = true;
-                }else{
-                    console.log("incorrect")
+                } else {
+                    // console.log("incorrect")
+                    animateCSS(dom, "headShake")
                     SoundWrong()
                 }
                 // Chequear si ya están todas adivinadas
-                let cantCorrect = myObjects.filter(function(e){ return e.correct }).length
+                let cantCorrect = myObjects.filter(function(e) {
+                    return e.correct
+                }).length
 
-                let cantGuessed = myObjects.filter(function(e){ return e.guessed }).length
-                if(cantGuessed == cantCorrect){
-                    console.log("Game won !")
+                let cantGuessed = myObjects.filter(function(e) {
+                    return e.guessed
+                }).length
+                if (cantGuessed == cantCorrect) {
+
+                    var self=this
+                    setTimeout(function(){
+                        console.log("show!")
+                        self.message.title = "Ganaste el primer juego"
+                        self.message.show = true
+                    }, 1200)
+                }
+            },
+            NextQuestion: function(answer = ""){
+                if(answer != ""){
+                    let currQuestion = this.games[this.capsule][this.game - 1].question
+                    let correctAnswer = this.games[this.capsule][this.game - 1].objects[currQuestion].r
+                    if(correctAnswer == answer){
+                        console.log("correct!")
+                        this.games[this.capsule][this.game - 1].score++
+                    }
+                }
+                let cantQuestions = this.games[this.capsule][this.game - 1].objects.length
+                let currCuestion =  this.games[this.capsule][this.game - 1].question
+                if(currCuestion == cantQuestions-1){
+                     // Game ended
+                     console.log("show!")
+                     if(this.capsule == "seigneurie" && app.game == 3){
+                         this.message.title = this.seigneurieJeu3Win
+                     }else{
+                         this.message.title = this.games[this.capsule][this.game - 1].winMessage
+                     }
+                     this.message.show = true
+
+                }else{
+                    this.games[this.capsule][this.game - 1].question++
                 }
 
+            },
 
+            NextJeux: function(){
+                    if(this.game == 3){
+                        this.capsule = "menu"
+                        this.game = 0
+                    }else{
+                        this.game++
+                    }
+                    this.message.show = false
+            },
+            MainMenu: function(){
+                this.capsule = "menu"
+                this.game = 0
             }
         },
 
@@ -89,12 +212,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     let soundCorrect = document.getElementById("soundCorrect");
-    function SoundCorrect(){
+
+    function SoundCorrect() {
         soundCorrect.play();
     }
     let soundWrong = document.getElementById("soundWrong");
-    function SoundWrong(){
+
+    function SoundWrong() {
         soundWrong.play();
     }
 
+
+
+
 }) //DOMContentLoaded
+
+
+const animateCSS = (element, animation, prefix = 'animate__') =>
+    // We create a Promise and return it
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        //const node = document.querySelector(element);
+
+        element.classList.add(`${prefix}animated`, animationName);
+
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            element.classList.remove(`${prefix}animated`, animationName);
+            resolve('Animation ended');
+        }
+
+        element.addEventListener('animationend', handleAnimationEnd, {
+            once: true
+        });
+    });
+
+var myMessage = document.getElementById("message")
+function ShowMessage(){
+
+    app._data.message.show = true
+    animateCSS(myMessage,"slideInLeft")
+}
+function HideMessage(){
+    animateCSS(myMessage,"slideOutRight").then((el) =>{app._data.message.show = false})
+}
